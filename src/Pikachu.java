87ec4886ac852259e416cfd43feb3e66856cd976
Arnab Das Utsa <144443522+iUtsa@users.noreply.github.com>
2025-02-28 21:16:15 +0000

    pikachu class updated

--- src/Pikachu.java
+++ src/Pikachu.java
@@ -5,36 +5,52 @@ public class Pikachu extends PokemonCard {
     private Attack thunderBall;
 
     public Pikachu() {
-        super("Pikachu", 60, "Electric", 20, "pikachu.png");
-        this.thunderShock = new Attack("Thunder Shock", 25);
-        this.thunderBall = new Attack("Thunder Balls", 30);
+        super("Pikachu", 60, "Electric", 20, "pikachu.png",
+                new Attack("Thunder Shock", 20),
+                new Attack("Thunder Ball", 25));
 
+        // Assign attacks from the superclass
+        this.thunderShock = getAttack1();
+        this.thunderBall = getAttack2();
     }
 
+    @Override
     public void useAttack(PokemonCard opponent) {
         if (getEnergy() >= 1) {
+            int damageDealt = thunderShock.getDamage();
             thunderShock.performAttack(this, opponent);
+            opponent.takeDamage(damageDealt);
 
-            //  10% chance to Paralyze the opponent
             if (Math.random() < 0.1) {
                 opponent.setStatusEffect("Paralyze");
                 System.out.println(opponent.getName() + " is now Paralyzed!");
             }
+
+            JOptionPane.showMessageDialog(null, this.getName() + " used Thunder Shock and dealt " + damageDealt + " damage to " + opponent.getName(),
+                    "Attack Info", JOptionPane.INFORMATION_MESSAGE);
+
             this.minusEnergy(1);
         }
     }
 
+    @Override
     public void useAttack2(PokemonCard opponent) {
         if (getEnergy() >= 2) {
+            int damageDealt = thunderBall.getDamage();
             thunderBall.performAttack(this, opponent);
+            opponent.takeDamage(damageDealt);
 
-            // 10% chance to Paralyze the opponent
             if (Math.random() < 0.1) {
                 opponent.setStatusEffect("Paralyze");
                 System.out.println(opponent.getName() + " is now Paralyzed!");
             }
 
+            JOptionPane.showMessageDialog(null, this.getName() + " used Thunder Ball and dealt " + damageDealt + " damage to " + opponent.getName(),
+                    "Attack Info", JOptionPane.INFORMATION_MESSAGE);
+
             this.minusEnergy(2);
         }
     }
+
 }
+
